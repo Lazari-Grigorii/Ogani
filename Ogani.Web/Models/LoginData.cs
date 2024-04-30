ff262868be8c108a300ffeb9da42119fd8c415eb
Nichita Belenciuc <nichita.belenciuc@isa.utm.md>
2024-04-30 23:15:24 +0300

    Some models creates and updates

--- Ogani.Web/Models/LoginData.cs
+++ Ogani.Web/Models/LoginData.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Ogani.Web.Models
+﻿namespace Ogani.Models
 {
-    public class LoginData
+    public class RegistrationModel
     {
         public string Username { get; set; }
-        public string Password { get; set; }
+
+        public string Password1 { get; set; }
+
+        public string Password2 { get; set; }
+
+        public string Email { get; set; }
     }
 }
