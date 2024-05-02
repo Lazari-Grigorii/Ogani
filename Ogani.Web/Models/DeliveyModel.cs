726ad1d991656dbac38f6deb8240759514924563
Nichita Belenciuc <nichita.belenciuc@isa.utm.md>
2024-05-02 17:18:11 +0300

    Add delivery model

---
+++ Ogani.Web/Models/DeliveyModel.cs
@@ -0,0 +1,14 @@
+﻿using Ogani.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ogani.Models
+{
+    public class DeliveryM
+    {
+        public int Id { get; set; }
+        public DeliveryType DeliveryType { get; set; }
+    }
+}
