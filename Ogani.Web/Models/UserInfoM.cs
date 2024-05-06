242eb02c0905bc41a174238e781d6ee04c136a74
Nichita Belenciuc <nichita.belenciuc@isa.utm.md>
2024-05-06 15:24:41 +0300

    add user info model

---
+++ Ogani.Web/Models/UserInfoM.cs
@@ -0,0 +1,23 @@
+﻿using Ogani.Domain.Entities.User;
+using Ogani.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ogani.Models
+{
+    public class UserInfoM
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public List<LoginHistoryD> LoginHistories { get; set; }
+
+        public Role UserRole { get; set; }
+
+    }
+}
