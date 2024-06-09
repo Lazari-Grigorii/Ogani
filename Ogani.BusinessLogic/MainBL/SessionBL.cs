d740bec913f3d1c81dd7026599f41c50dc2acc7c
anastasia <anastasiia.moshniaha@isa.utm.md>
2024-06-09 14:32:28 +0300

    Добавлена логика сессий

--- Ogani.BusinessLogic/MainBL/SessionBL.cs
+++ Ogani.BusinessLogic/MainBL/SessionBL.cs
@@ -1,27 +1,40 @@
-﻿using Ogani.BusinessLogic.Core;
-using Ogani.BusinessLogic.Interfaces;
-using Ogani.Domain.Entities.AuthorizationEntity;
-using Ogani.Domain.Entities.GeneralResponse;
+﻿using Ogani.BusinessLogic.Interfaces;
+using Ogani.BussinesLogic.Core;
 using Ogani.Domain.Entities.User;
-using Ogani.Domain.Entities.User.DbModel;
+using Ogani.Domain.Entities.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
-namespace Ogani.BusinessLogic.MainBL
+namespace Ogani.BussinesLogic
 {
     public class SessionBL : UserAPI, ISession
     {
-        public RequestResponseData UserLoginAction(ULoginData data)
+        public ResponseData UserLogin(LoginData data)
         {
-            return ULASessionCheck(data);
+            return UserValidateSession(data);
         }
 
-        public UCookieData GenGookieAlgorithm(User dataUser)
+        public ResponseRegisterData UserRegister(RegisterData userRegisterData)
         {
-            return UCGenerationAlg(dataUser);
+            return UserRegisterAction(userRegisterData);
+        }
+
+        public HttpCookie GenCookie(string loginCredential)
+        {
+            return Cookie(loginCredential);
+        }
+
+        public UserMinimal GetUserByCookie(string apiCookieValue)
+        {
+            return UserCookie(apiCookieValue);
+        }
+        public ResponseData UserLogout()
+        {
+            return UserLogoutAction();
         }
     }
 }
